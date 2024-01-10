2e35bb62200b438c93ee84793cac13920cf8bb0f
yuhaigiub <buigiahuynt@gmail.com>
2024-01-10 12:22:49 +0700

    beatsODE3

---
+++ beatsODE3/layers.py
@@ -0,0 +1,128 @@
+import torch
+import torch.nn.functional as F
+from torch import nn, Tensor
+
+import torchdiffeq
+
+class nconv(nn.Module):
+    def __init__(self):
+        super(nconv,self).__init__()
+
+    def forward(self, x: Tensor, A: Tensor):
+        # x.shape = (batch, channels, nodes, time_steps)
+        # A.shape = (node, node)
+        x = torch.einsum('ncwl,vw->ncvl', (x, A))
+        return x.contiguous()
+
+class linear(nn.Module):
+    def __init__(self, in_dim, out_dim, bias=True):
+        super(linear,self).__init__()
+        self.mlp = nn.Conv2d(in_dim, out_dim, kernel_size=(1, 1), padding=(0, 0), stride=(1, 1), bias=bias)
+    
+    def forward(self, x: Tensor):
+        return self.mlp(x)
+
+class dilated_inception(nn.Module):
+    def __init__(self, in_dim, out_dim, dilation_factor=1):
+        super(dilated_inception, self).__init__()
+        self.tconv = nn.ModuleList()
+        self.kernels = [2, 3, 6, 7]
+        out_dim = int(out_dim / len(self.kernels))
+        for kern in self.kernels:
+            self.tconv.append(nn.Conv2d(in_dim, out_dim, (1, kern), dilation=(1, dilation_factor)))
+    
+    def forward(self, input: Tensor):
+        x = []
+        for i in range(len(self.kernels)):
+            output = self.tconv[i](input)
+            x.append(output)
+        
+        for i in range(len(self.kernels)):
+            x[i] = x[i][..., -x[-1].size(3):]
+        
+        
+        x = torch.cat(x, dim=1)
+        return x
+
+class CGPBlock(nn.Module):
+    def __init__(self, in_dim, out_dim, alpha=1.0):
+        super(CGPFunc, self).__init__()
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.alpha = alpha
+        self.conv = nconv()
+    
+    def forward(self, x: Tensor, adj: Tensor):
+        adj = adj + torch.eye(adj.size(0)).to(x.device)
+        d = adj.sum(1)
+        _d = torch.diag(torch.pow(d, -0.5))
+        adj_norm = torch.mm(torch.mm(_d, adj), _d)
+        
+        ax = self.conv(x, adj_norm)
+        f = 0.5 * self.alpha * (ax - x)
+        
+        return f
+
+class CGPODEBlock(nn.Module):
+    def __init__(self, in_dim, out_dim, time, step_size, alpha=1.0):
+        super(CGPODEBlock, self).__init__()
+        self.time = time
+        self.step_size = step_size
+        
+        self.estimated_nfe = round(time / step_size)
+        
+        self.odefunc = CGPFunc(in_dim, out_dim, alpha)
+        
+        self.mlp = linear((self.estimated_nfe + 1) * in_dim, out_dim)
+    
+    def forward(self, x: Tensor, adj: Tensor):
+        self.odefunc.set_x0(x)
+        self.odefunc.set_adj(adj)
+        
+        self.integration_time = torch.tensor([0, self.time]).float().type_as(x)
+        out = torchdiffeq.odeint(self.odefunc, 
+                                 x, 
+                                 self.integration_time,
+                                 method="euler",
+                                 options=dict(step_size=self.step_size))
+        
+        outs = self.odefunc.out
+        self.odefunc.out = []
+        outs.append(out[-1])
+        
+        h_out = torch.cat(outs, dim=1)
+        h_out = self.mlp(h_out)
+        
+        return h_out
+
+class CGPFunc(nn.Module):
+    def __init__(self, in_dim, out_dim, alpha=1.0):
+        super(CGPFunc, self).__init__()
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.alpha = alpha
+        self.conv = nconv()
+        
+        self.out = []
+        
+        self.x0 = None
+        self.adj = None
+    
+    def set_x0(self, x0: Tensor):
+        self.x0 = x0.clone().detach()
+    
+    def set_adj(self, adj: Tensor):
+        self.adj = adj
+    
+    def forward(self, t, x: Tensor):
+        adj = adj + torch.eye(adj.size(0)).to(x.device)
+        d = adj.sum(1)
+        _d = torch.diag(torch.pow(d, -0.5))
+        adj_norm = torch.mm(torch.mm(_d, adj), _d)
+        
+        self.out.append(x)
+        
+        ax = self.conv(x, adj_norm)
+        f = 0.5 * self.alpha * (ax - x)
+        
+        return f
--- beatsODE3/model.py
+++ beatsODE3/model.py
@@ -13,9 +13,9 @@ class BeatsODE2(nn.Module):
                  seq_len=12,
                  time_1=1.0, step_size_1=0.25,
                  time_2=1.2, step_size_2=0.4,
-                 share_weight_in_stack=True):
+                 share_weight_in_stack=False):
         super(BeatsODE2, self).__init__()
-        print('BeatsODE2_1')
+        print('BeatsODE3')
         if share_weight_in_stack:
             print('BeatsODE with share_stack_weight')
         
@@ -50,6 +50,128 @@ class BeatsODE2(nn.Module):
         
         return forecast
 
+class BeatsODE3(nn.Module):
+    def __init__(self,
+                 in_dim=2,
+                 out_dim=2,
+                 seq_len=12,
+                 time_0=1.2, step_size_0=0.4,
+                 time_1=1.0, step_size_1=0.25,
+                 time_2=1.2, step_size_2=0.4,
+                 share_weight_in_stack=False):
+        super(BeatsODE3, self).__init__()
+        print('BeatsODE3')
+        if share_weight_in_stack:
+            print('BeatsODE with share_stack_weight')
+        
+        self.time = time_0
+        self.step_size = step_size_0
+        
+        n_stacks = 3
+        self.stacks = nn.ModuleList()
+        
+        for stack_id in range(n_stacks):
+            self.stacks.append(BeatsODEBlock(in_dim, out_dim, seq_len, time_1, step_size_1, time_2, step_size_2))
+    
+    def forward(self, backcast: Tensor, adj: Tensor):
+        
+        self.integration_time = torch.tensor([0, self.time]).float().type_as(backcast)
+        forecast = torch.zeros_like(backcast).type_as(backcast)
+        
+        for stack_id in range(len(self.stacks)):
+            stack: BeatsODEBlock = self.stacks[stack_id]
+            
+            stack.set_adj(adj)
+            stack.set_forecast(forecast)
+            
+            b: Tensor = torchdiffeq.odeint(stack,
+                                           backcast,
+                                           self.integration_time,
+                                           method="euler",
+                                           options=dict(step_size=self.step_size))[-1]
+            backcast = b - stack.backcast
+            forecast = stack.forecast.clone()
+            
+            stack.backcast = None
+            stack.forecast = None
+            
+        backcast = backcast.transpose(1, 3)
+        forecast = forecast.transpose(1, 3)
+        
+        return forecast
+
+class BeatsODEBlock(nn.Module):
+    def __init__(self, 
+                 in_dim, 
+                 out_dim, 
+                 seq_len,
+                 time_1, step_size_1,
+                 time_2, step_size_2,
+                 conv_dim=32,
+                 end_dim=128):
+        super(BeatsODEBlock, self).__init__()
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        
+        self.seq_len = seq_len
+        self.nfe = round(time_1 / step_size_1)
+        max_kernel_size = 7
+        self.receptive_field = self.nfe * (max_kernel_size - 1) + out_dim
+        
+        self.start_conv = nn.Conv2d(in_dim, conv_dim, kernel_size=(1, 1))
+        self.st_block = STBlock(self.receptive_field, 1, conv_dim, time_2, step_size_2)
+        
+        self.backcast_decoder = nn.Sequential(nn.Conv2d(conv_dim, end_dim, kernel_size=(1, 1)),
+                                              nn.ReLU(),
+                                              nn.Conv2d(end_dim, seq_len, kernel_size=(1, 1)))
+        
+        self.forecast_decoder = nn.Sequential(nn.Conv2d(conv_dim, end_dim, kernel_size=(1, 1)),
+                                              nn.ReLU(),
+                                              nn.Conv2d(end_dim, seq_len, kernel_size=(1, 1)))
+        
+        self.backcast = None
+        self.forecast = None
+        self.adj = None
+        
+    def set_backcast(self, backcast: Tensor):
+        if backcast is not None:
+            self.backcast = backcast
+        else:
+            backcast = None
+    
+    def set_forecast(self, forecast: Tensor):
+        self.forecast = forecast
+    
+    def set_adj(self, adj: Tensor):
+        self.adj = adj
+    
+    def forward(self, t, x: Tensor):
+        if self.backcast is None:
+            self.set_backcast(x)
+        else:
+            saved_backcast = self.backcast
+            self.set_backcast(x)
+            x = x - saved_backcast
+        
+        if self.seq_len < self.receptive_field:
+            x = F.pad(x, (self.receptive_field - self.seq_len, 0))
+        
+        x = self.start_conv(x)
+        
+        x = self.st_block(x, self.adj)
+        self.st_block.set_intermediate(1)
+        
+        x = x[..., -self.out_dim:]
+        x = F.layer_norm(x, tuple(x.shape[1:]), weight=None, bias=None, eps=1e-5)
+        
+        # decoder
+        backcast = self.backcast_decoder(x).transpose(1, 3)
+        forecast = self.forecast_decoder(x).transpose(1, 3)
+        
+        self.set_forecast(self.forecast + forecast)
+        
+        return backcast
+
 class BeatsBlock(nn.Module):
     def __init__(self,
                  in_dim,
@@ -70,9 +192,7 @@ class BeatsBlock(nn.Module):
         
         self.start_conv = nn.Conv2d(in_dim, conv_dim, kernel_size=(1, 1))
         
-        self.ODE = ODEBlock(STBlock(self.receptive_field, 1, conv_dim, time_2, step_size_2), 
-                            time_1, 
-                            step_size_1)
+        self.st_block = STBlock(self.receptive_field, 1, conv_dim, time_2, step_size_2)
         
         self.backcast_decoder = nn.Sequential(nn.Conv2d(conv_dim, end_dim, kernel_size=(1, 1)),
                                               nn.ReLU(),
@@ -88,9 +208,8 @@ class BeatsBlock(nn.Module):
         
         x = self.start_conv(x)
         
-        self.ODE.odefunc.set_adj(adj)
-        x = self.ODE(x)
-        self.ODE.odefunc.set_intermediate(1)
+        x = self.st_block(x, adj)
+        self.st_block.set_intermediate(1)
         
         x = x[..., -self.out_dim:]
         x = F.layer_norm(x, tuple(x.shape[1:]), weight=None, bias=None, eps=1e-5)
@@ -100,23 +219,6 @@ class BeatsBlock(nn.Module):
         forecast = self.forecast_decoder(x)
         
         return backcast, forecast
-
-class ODEBlock(nn.Module):
-    def __init__(self, odefunc, time_1, step_size_1):
-        super(ODEBlock, self).__init__()
-        self.odefunc = odefunc
-        
-        self.time = time_1
-        self.step_size = step_size_1
-    
-    def forward(self, x: Tensor):
-        self.integration_time = torch.tensor([0, self.time]).float().type_as(x)
-        out = torchdiffeq.odeint(self.odefunc, 
-                                 x, 
-                                 self.integration_time,
-                                 method="euler",
-                                 options=dict(step_size=self.step_size))
-        return out[-1]
 
 class STBlock(nn.Module):
     def __init__(self, 
@@ -137,17 +239,12 @@ class STBlock(nn.Module):
         
         self.gconv1 = CGPODEBlock(hidden_dim, hidden_dim, time_2, step_size_2)
         self.gconv2 = CGPODEBlock(hidden_dim, hidden_dim, time_2, step_size_2)
-        
-        self.adj = None
-    
-    def set_adj(self, adj: Tensor):
-        self.adj = adj
     
     def set_intermediate(self, dilation):
         self.new_dilation = dilation
         self.intermediate_seq_len = self.receptive_field
     
-    def forward(self, t, x: Tensor):
+    def forward(self, x: Tensor, adj: Tensor):
         x = x[..., -self.intermediate_seq_len:]
         
         for tconv in self.inception_1.tconv:
@@ -168,7 +265,7 @@ class STBlock(nn.Module):
         
         x = F.dropout(x, self.dropout)
         
-        x = self.gconv1(x, self.adj) + self.gconv2(x, self.adj.T)
+        x = self.gconv1(x, adj) + self.gconv2(x, adj.T)
         
         x = F.pad(x, (self.receptive_field - x.size(3), 0))
         
